bf01e91d7d6bccb2d8a11fe958ebafcc767203be
Luke Winkenbach <lukewink@gmail.com>
2017-03-06 22:24:02 -0800

    Added groovydoc and fixed indentation issues

--- src/main/groovy/com/lwink/gradle/buildops/BuildopsPlugin.groovy
+++ src/main/groovy/com/lwink/gradle/buildops/BuildopsPlugin.groovy
@@ -5,6 +5,9 @@ import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.Method.POST
 import static groovyx.net.http.ContentType.JSON
 
+/**
+ * Plugin implementation that allows for easy interaction with buildops servers.
+ */
 class BuildopsPlugin implements Plugin<Project> {
   static final String TASK_NAME = "getBuildNumber"
   static final String BUILDOPS_URL = "buildopsUrl"
@@ -13,11 +16,16 @@ class BuildopsPlugin implements Plugin<Project> {
   static final String BUILDOPS_REVISION = "buildopsRevision"
   static final String BUILDOPS_BRANCH = "buildopsBranch"
 
+  /**
+   * Define gradle tasks to assist with interacting with buildops servers.
+   * 
+   * @param project Gradle project calling the plugin.
+   */
   def void apply(Project project) {
     project.task(TASK_NAME) {
       description "Get the build number from a buildops server."
       doLast {
-				// Check that the required properties have been set
+        // Check that the required properties have been set
         if (!project.hasProperty(BUILDOPS_URL)) {
           throw new InvalidUserDataException("Project property ${BUILDOPS_URL} is not set")
         }
@@ -28,12 +36,12 @@ class BuildopsPlugin implements Plugin<Project> {
           throw new InvalidUserDataException("Project property ${BUILDOPS_VERSION_BASE} is not set")
         }
 
-				HTTPBuilder httpBuilder = new HTTPBuilder(project.buildopsUrl)
-				httpBuilder.request(POST, JSON) {
-					uri.path = "/api/builds"
+        HTTPBuilder httpBuilder = new HTTPBuilder(project.buildopsUrl)
+        httpBuilder.request(POST, JSON) {
+          uri.path = "/api/builds"
 
           // Add the required fields component and version_base
-				  body = [component: project.getProperty(BUILDOPS_COMPONENT), 
+          body = [component: project.getProperty(BUILDOPS_COMPONENT), 
                   version_base: project.getProperty(BUILDOPS_VERSION_BASE)]
 
           // Now add the optional fields
@@ -44,13 +52,13 @@ class BuildopsPlugin implements Plugin<Project> {
             body.branch = project.getProperty(BUILDOPS_BRANCH)
           }
 
-					response.success = { resp, build ->
-					  assert resp.statusLine.statusCode == 201
-				    project.ext.set("buildopsBuildNumber", build.number)
+          response.success = { resp, build ->
+            assert resp.statusLine.statusCode == 201
+            project.ext.set("buildopsBuildNumber", build.number)
           } 
         }
-			}
-		}
+      }
+    }
   }
 }
 
